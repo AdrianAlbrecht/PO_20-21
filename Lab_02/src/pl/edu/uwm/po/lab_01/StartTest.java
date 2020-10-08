9ff93f94a4f4df46fa83b4ff7fb42e30694ae644
AdrianAlbrecht <155376@student.uwm.edu.pl>
2020-10-08 16:44:36 +0200

    Lab 02

--- Lab_02/src/pl/edu/uwm/po/lab_01/StartTest.java
+++ Lab_02/src/pl/edu/uwm/po/lab_01/StartTest.java
@@ -3,26 +3,29 @@ import java.util.Scanner;
 
 public class StartTest {
     public static void main(String[] args) {
-        Scanner klawiatura = new Scanner(System.in);
         Start z1 = new Start();
-        z1.z01a(Integer.parseInt(klawiatura.nextLine()));
+        z1.z01a(pobierz());
         z1.wypisz(false);
-        z1.z01b(Integer.parseInt(klawiatura.nextLine()));
+        z1.z01b(pobierz());
         z1.wypisz(false);
-        z1.z01c(Integer.parseInt(klawiatura.nextLine()));
+        z1.z01c(pobierz());
         z1.wypisz(false);
-        z1.z01d(Integer.parseInt(klawiatura.nextLine()));
+        z1.z01d(pobierz());
         z1.wypisz(false);
-        z1.z01e(Integer.parseInt(klawiatura.nextLine()));
+        z1.z01e(pobierz());
         z1.wypisz(false);
-        z1.z01f(Integer.parseInt(klawiatura.nextLine()));
+        z1.z01f(pobierz());
         z1.wypisz(false);
-        z1.z01g(Integer.parseInt(klawiatura.nextLine()));
+        z1.z01g(pobierz());
         z1.wypisz(true);
-        z1.z01h(Integer.parseInt(klawiatura.nextLine()));
+        z1.z01h(pobierz());
         z1.wypisz(false);
-        z1.z01i(Integer.parseInt(klawiatura.nextLine()));
+        z1.z01i(pobierz());
         z1.wypisz(false);
-        Zad2 z2 = new Zad2(Integer.parseInt(klawiatura.nextLine()));
+        Zad2 z2 = new Zad2(pobierz());
+    }
+    public static int pobierz(){
+        Scanner klawiatura = new Scanner(System.in);
+        return Integer.parseInt(klawiatura.nextLine());
     }
 }
