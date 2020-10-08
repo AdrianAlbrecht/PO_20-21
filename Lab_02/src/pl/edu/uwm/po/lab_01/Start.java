57da90958ea742deb56a1bc5fd6d74b149821736
AdrianAlbrecht <155376@student.uwm.edu.pl>
2020-10-08 16:32:45 +0200

    Lab 02

--- Lab_02/src/pl/edu/uwm/po/lab_01/Start.java
+++ Lab_02/src/pl/edu/uwm/po/lab_01/Start.java
@@ -2,67 +2,67 @@ package pl.edu.uwm.po.lab_01;
 import java.util.Scanner;
 
 public class Start {
-    double s=0;
-    double s2=1;
-    Scanner myObj = new Scanner(System.in);
+    double wynik=0;
+    double wynik2=1;
+    Scanner klawiatura = new Scanner(System.in);
     public void wypisz(boolean a){
-        System.out.println(this.s);
+        System.out.println(this.wynik);
         if(a){
-            System.out.println(this.s2);
+            System.out.println(this.wynik2);
         }
     }
     public void z01a(int n){
         for (int i=0; i<n;i++){
-            this.s+=Double.parseDouble(myObj.nextLine());
+            this.wynik+=Double.parseDouble(klawiatura.nextLine());
         }
     }
     public void z01b(int n){
-        this.s=1;
+        this.wynik=1;
         for (int i=0; i<n;i++){
-            this.s*=Double.parseDouble(myObj.nextLine());
+            this.wynik*=Double.parseDouble(klawiatura.nextLine());
         }
     }
     public void z01c(int n){
         for (int i=0; i<n;i++){
-            this.s+=Math.abs(Double.parseDouble(myObj.nextLine()));
+            this.wynik+=Math.abs(Double.parseDouble(klawiatura.nextLine()));
         }
     }
     public void z01d(int n){
         for (int i=0; i<n;i++){
-            this.s+=Math.sqrt(Math.abs(Double.parseDouble(myObj.nextLine())));
+            this.wynik+=Math.sqrt(Math.abs(Double.parseDouble(klawiatura.nextLine())));
         }
     }
     public void z01e(int n){
-        this.s=1;
+        this.wynik=1;
         for (int i=0; i<n;i++){
-            this.s*=Math.abs(Double.parseDouble(myObj.nextLine()));
+            this.wynik*=Math.abs(Double.parseDouble(klawiatura.nextLine()));
         }
     }
     public void z01f(int n){
         for (int i=0; i<n;i++){
-            double a=Double.parseDouble(myObj.nextLine());
-            this.s+=Math.pow(a,2);
+            double a=Double.parseDouble(klawiatura.nextLine());
+            this.wynik+=Math.pow(a,2);
         }
     }
     public void z01g(int n){
         for (int i=0; i<n;i++){
-            double a=Double.parseDouble(myObj.nextLine());
-            this.s+=a;
-            this.s2*=a;
+            double a=Double.parseDouble(klawiatura.nextLine());
+            this.wynik+=a;
+            this.wynik2*=a;
         }
     }
     public void z01h(int n){
         for (int i=1; i<=n;i++){
-            double a=Double.parseDouble(myObj.nextLine());
-            this.s+=Math.pow(-1,i+1)*a;
+            double a=Double.parseDouble(klawiatura.nextLine());
+            this.wynik+=Math.pow(-1,i+1)*a;
         }
     }
     public void z01i(int n){
         int wyk=1;
         for (int i=0; i<n;i++){
-            double a=Double.parseDouble(myObj.nextLine());
+            double a=Double.parseDouble(klawiatura.nextLine());
             wyk*=(i+1);
-            this.s+=(Math.pow(-1,i)*a)/wyk;
+            this.wynik+=(Math.pow(-1,i)*a)/wyk;
         }
     }
 }
--- Lab_02/src/pl/edu/uwm/po/lab_01/StartTest.java
+++ Lab_02/src/pl/edu/uwm/po/lab_01/StartTest.java
@@ -3,35 +3,25 @@ import java.util.Scanner;
 
 public class StartTest {
     public static void main(String[] args) {
-        Scanner myObj = new Scanner(System.in);
-        Start z1 = new Start();
-        z1.z01a(Integer.parseInt(myObj.nextLine()));
+        Scanner klawiatura = new Scanner(System.in);
+        z1.z01a(Integer.parseInt(klawiatura.nextLine()));
         z1.wypisz(false);
-        z1.s=0;
-        z1.z01b(Integer.parseInt(myObj.nextLine()));
+        z1.z01b(Integer.parseInt(klawiatura.nextLine()));
         z1.wypisz(false);
-        z1.s=0;
-        z1.z01c(Integer.parseInt(myObj.nextLine()));
+        z1.z01c(Integer.parseInt(klawiatura.nextLine()));
         z1.wypisz(false);
-        z1.s=0;
-        z1.z01d(Integer.parseInt(myObj.nextLine()));
+        z1.z01d(Integer.parseInt(klawiatura.nextLine()));
         z1.wypisz(false);
-        z1.s=0;
-        z1.z01e(Integer.parseInt(myObj.nextLine()));
+        z1.z01e(Integer.parseInt(klawiatura.nextLine()));
         z1.wypisz(false);
-        z1.s=0;
-        z1.z01f(Integer.parseInt(myObj.nextLine()));
+        z1.z01f(Integer.parseInt(klawiatura.nextLine()));
         z1.wypisz(false);
-        z1.s=0;
-        z1.z01g(Integer.parseInt(myObj.nextLine()));
+        z1.z01g(Integer.parseInt(klawiatura.nextLine()));
         z1.wypisz(true);
-        z1.s=0;
-        z1.s2=1;
-        z1.z01h(Integer.parseInt(myObj.nextLine()));
+        z1.z01h(Integer.parseInt(klawiatura.nextLine()));
         z1.wypisz(false);
-        z1.s=0;
-        z1.z01i(Integer.parseInt(myObj.nextLine()));
+        z1.z01i(Integer.parseInt(klawiatura.nextLine()));
         z1.wypisz(false);
-        z1.s=0;
+        Zad2 z2 = new Zad2(Integer.parseInt(klawiatura.nextLine()));
     }
 }
